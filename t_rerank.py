1592861cb92641e0c14a3553fe03535d5ba3af19
QuagHien <hqh2042003@gmail.com>
2024-09-24 01:57:18 +0700

    new update

--- t_rerank.py
+++
@@ -1,6 +0,0 @@
-from marketAgent.src.modeling.rerank import ReRank
-
-text = ["hello", "friend"]
-rerank_pytorch = ReRank('model_repository/mbert-rerank-onnx/1')
-logit = rerank_pytorch.encode(text=text, max_length=128)
-print("logit: ", logit)
